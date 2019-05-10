b01a6eade824dd24bc6aececf4c631332764a509
Mike Garringer <michaelleelyngarringer@gmail.com>
2019-05-10 14:09:50 -0500

    Spotify search working but with some issues - what if two artists have a track with the same name or the song title is multiple words long?

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: "f6e7070eaff542468dfe97def9c7eb40",
+  secret: "671b966a3940424188457e40b1d19bfc"
 };
 
 // axios.get("http://www.omdbapi.com/?t=s&y=&&apikey=trilogy").then
--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 var fs = require("fs")
+var Spotify = require('node-spotify-api');
 var keys = require("./keys")
 
 //Vars for user input//
@@ -9,23 +10,48 @@ var query = input[3]
 
 
 //concert-this
-if (command === "concert-this"){
+if (command === "concert-this") {
     console.log("Concert")
 }
 //If the command is something different that concert-this run the correct command
-switch(command){
-//spotify-this-song
-case "spotify-this-song":
-console.log("spotify")
-break;
+switch (command) {
+    //spotify-this-song
+    case "spotify-this-song":
+        spotify(query);
+        break;
 
-//movie-this
-case "movie-this":
-console.log("Movies")
-break;
+    //movie-this
+    case "movie-this":
+        console.log("Movies")
+        break;
 
-//do-what-it-says
-case "do-what-it-says":
-console.log("Text")
-break;
+    //do-what-it-says
+    case "do-what-it-says":
+        console.log("Text")
+        break;
 };
+
+
+
+//Spotify function
+function spotify(query) {
+    var spotify = new Spotify(keys.spotify);
+    if (!query) {
+        query = "The Sign"
+    }
+    spotify.search({ type: 'track', query: query }, function (err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+     var song = data.tracks.items;
+        console.log(
+            `
+Song: ${song[0].name}
+Artist:${song[0].artists[0].name}
+Album: ${song[0].album.name}
+Preview link: ${song[0].preview_url}
+  `
+        )
+    });
+}
