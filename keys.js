df2c2adea2fe9f8504e8f33f1146bd29e4c51a79
Mike Garringer <michaelleelyngarringer@gmail.com>
2019-05-10 13:47:59 -0500

    have placeholders for the commands that will run when user input is inserted

--- keys.js
+++ keys.js
@@ -4,3 +4,5 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+// axios.get("http://www.omdbapi.com/?t=s&y=&&apikey=trilogy").then
--- liri.js
+++ liri.js
@@ -1,8 +1,31 @@
 require("dotenv").config();
+var fs = require("fs")
 var keys = require("./keys")
-var spotify = new Spotify(keys.spotify);
+
+//Vars for user input//
+var input = process.argv
+var command = input[2]
+var query = input[3]
+
 
 //concert-this
+if (command === "concert-this"){
+    console.log("Concert")
+}
+//If the command is something different that concert-this run the correct command
+switch(command){
 //spotify-this-song
+case "spotify-this-song":
+console.log("spotify")
+break;
+
 //movie-this
+case "movie-this":
+console.log("Movies")
+break;
+
 //do-what-it-says
+case "do-what-it-says":
+console.log("Text")
+break;
+};
